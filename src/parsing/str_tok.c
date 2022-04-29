94789d084943157bb14acc63105ef43b21c54a65
ChoZeur <flcarval@student.42.fr>
2022-04-29 17:00:10 +0200

    str_tok wip

--- src/parsing/str_tok.c
+++ src/parsing/str_tok.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 16:45:12 by flcarval          #+#    #+#             */
-/*   Updated: 2022/04/27 17:13:50 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/04/29 16:43:25 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ t_tok	*str_tok(char *str)
 
 static int	identify_tok(char c)
 {
-	if (c == ' ')
+	// added tabs to SPACE type
+	if (c == ' ' || '\t')
 		return (SPACE);
 	else if (c == '>')
 		return (OUTREDIR);
@@ -53,6 +54,19 @@ static int	count_tok(char *str)
 	i = 0;
 	while (str[i])
 	{
-		
+		if (identify_tok(str[i]) == OUTREDIR)
+			// check if it's a single or double (> || >>)
+			if (identify_tok(str[i + 1]) == OUTREDIR)
+				i++;
+		else if (identify_tok(str[i]) == INREDIR)
+			// check if it's a single or double (< || <<)
+			if (identify_tok(str[i]) == INREDIR)
+				i++;
+		else if (identify_tok(str[i]) == LITERAL)
+			// go to end of LITERAL token
+			while (identify_tok(str[i]) == LITERAL)
+				i++;
+		res++;
+		i++;
 	}
 }

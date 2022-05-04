03a46ab3670735f10e20b1a46b45209f87c933cd
Titouan BREBION <tbrebion@e1r5p19.clusters.42paris.fr>
2022-05-04 15:09:13 +0200

    wip

--- src/display/display_prompt.c
+++ src/display/display_prompt.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 17:54:00 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/04 14:26:14 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/04 15:07:43 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 void	display_prompt(char **my_env)
 {
 	char	*cwd;
-	char	*user_line;
+	//char	*user_line;
 	//char	*to_print;
+	//int		i;
 	char	buff[4096 + 1];
+	(void)my_env;
 
+	//i = 0;
 	cwd = getcwd(buff, 4096);
-	user_line = catch_user(my_env);
-	while ((ft_strnstr(cwd, catch_username(user_line), ft_strlen(cwd))))
-		cwd++;
-	ft_printf("%s", cwd);
+	/*user_line = catch_user(my_env);
+	while ((ft_strnstr(cwd, catch_username(user_line)user_line + 5, ft_strlen(cwd))))
+		i++;*/
+	ft_printf("%s", cwd /*+ i*/);
 	ft_putstr(" \033[31m︻\033[0m\033[32m┳\033[0m\033[33mデ");
 	ft_putstr("\033[0m\033[34m═\033[0m\033[35m—\033[0m$ ");
 }

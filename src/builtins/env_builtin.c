e247724dc92c76a4e67da0ccd641f1e9b37d00d5
Titouan BREBION <tbrebion@e1r5p8.clusters.42paris.fr>
2022-05-06 16:14:30 +0200

    wip

--- src/builtins/env_builtin.c
+++ src/builtins/env_builtin.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 13:37:33 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/02 15:21:53 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/06 16:05:19 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ void	print_env(char **my_env)
 {
 	int	i;
 
-	i = -1;
-	while(my_env[++i])
+	i = 0;
+	while(my_env[i])
 	{
 		ft_printf("%s\n", my_env[i]);
+		i++;
 	}
 }
--- src/builtins/exit_shell.c
+++ src/builtins/exit_shell.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 09:32:37 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/06 15:56:19 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/06 16:07:08 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	exit_shell(char **my_env)
 {
 	int	i;
 
-	i = -1;
-	if (my_env)
+	i = 0;
+	if (my_env[i])
 	{
-		while (my_env[++i])
+		while (my_env[i])
 		{
 			free(my_env[i]);
+			i++;
 		}
 		free(my_env);
 	}

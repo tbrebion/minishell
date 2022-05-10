7f590c24e67ff5e17bf1dca10aaf38c88022dbc3
Titouan BREBION <tbrebion@e1r5p12.clusters.42paris.fr>
2022-05-10 13:39:05 +0200

    wip

--- src/display/display_prompt.c
+++ src/display/display_prompt.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 17:54:00 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/10 11:29:00 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/10 13:35:05 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,10 @@ char	*display_prompt(char **my_env)
 	while (cwd[i] != '/')
 		i++;
 	i++;
-	tmp_ret = ft_strdup(" \033[31m︻\033[0m\033[32m┳\033[0m\033[33mデ\033[0m\033[34m═\033[0m\033[35m—\033[0m$ ");
+	//tmp_ret = ft_strdup(" \033[31m︻\033[0m\033[32m┳\033[0m\033[33mデ\033[0m\033[34m═\033[0m\033[35m—\033[0m$ ");
+	tmp_ret = " \033[31m︻\033[0m\033[32m┳\033[0m\033[33mデ\033[0m\033[34m═\033[0m\033[35m—\033[0m$ ";
 	tmp_2_ret = ft_strjoin(cwd + i, tmp_ret); 
-	tmp_3_ret = ft_strdup("~/");
+	tmp_3_ret = "~/";//ft_strdup("~/");
 	ret = ft_strjoin(tmp_3_ret, tmp_2_ret);
 	//free(tmp_ret);
 	//free(tmp_2_ret);

bfcff86dfb9d5afd3fcce4b4c57379c197451c0a
Titouan BREBION <tbrebion@e1r5p17.clusters.42paris.fr>
2022-05-18 15:17:51 +0200

    15:17

--- src/utils/pipe.c
+++ src/utils/pipe.c
@@ -6,12 +6,12 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:30:45 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/12 13:50:40 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/18 15:15:50 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
+/*
 void	child_process(char **split_input, char **my_env, int *fd)
 {
 	int	filein;
@@ -22,7 +22,7 @@ void	child_process(char **split_input, char **my_env, int *fd)
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(filein, STDIN_FILENO);
 	close(fd[0]);
-	exexute(split_input[1], my_env);
+	//exexute(split_input[1], my_env);
 }
 
 void	parent_process(char **split_input, char **my_env, int *fd)
@@ -54,3 +54,47 @@ int	ft_pipe(char **split_input, char **my_env)
 		parent_process(split_input, my_env, fd);
 	return (0);
 }
+*/
+static void	child_process(t_data *data, int i)
+{
+	int	pid;
+	int fd[2];
+
+	if (pipe(fd) == -1)
+		return ;
+	pid = fork();
+	if (pid == -1)
+		return ;
+	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		execute(Cli[i], data->my_env);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		waitpid(pid, NULL, 0);
+	}
+}
+
+void	pipe(t_data *data, int nb_tok)
+{
+	int	i;
+	int	n;
+	int	filein;
+	int	filout;
+
+	i = 2;
+	filein = open(/*split_input[0]*/, O_RDONLY, 0777);
+	if (filein == -1)
+		return ;
+	fileout = open(/*split_input[3]*/, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fileout == -1)
+		return ;
+	dup2(filein, STDIN_FILENO);
+	while (i < nb_tok - 2)
+		child_process(data, n);
+	execute(Cli[i], data->my_env);
+}	
--- src/utils/signal_manager.c
+++ src/utils/signal_manager.c
@@ -6,18 +6,18 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:14:37 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/06 09:56:58 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/18 12:32:36 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-/*void	signal_handler(int signo, char **my_env)
+/*
+void	signal_handler(int signo)
 {
 	if (signo == SIGINT)
 	{
 		write(1, "\n", 1);
-		display_prompt(my_env);
+		//display_prompt(my_env);
 		signal(SIGINT, (__sighandler_t)signal_handler);
 	}
 }

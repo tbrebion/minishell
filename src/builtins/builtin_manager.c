d065c13f6586045f42715a64245d226472ba4572
Titouan BREBION <tbrebion@e1r7p16.42.fr>
2022-05-23 14:06:01 +0200

    14:05

--- src/builtins/builtin_manager.c
+++ src/builtins/builtin_manager.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_manager.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 11:19:52 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/18 12:07:50 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/05/23 14:00:21 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	builtin_manager(t_data *data, int i)
 {
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "env", 3) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "env", 4) == 0))
 		print_env(data->my_env);
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "exit", 4) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "exit", 5) == 0))
 		exit_shell(data->my_env);
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "pwd", 3) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "pwd", 4) == 0))
 		print_cwd();
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "echo", 4) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "echo", 5) == 0))
 		echo_builtin(data, i);
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "export", 6) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "export", 7) == 0))
 		export_varenv(data, i);
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "cd", 2) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "cd", 3) == 0))
 		cd_builtin(data, i);
-	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "unset", 5) == 0))
+	if ((size_t)(ft_strncmp(get_n_lst(data->Tokens, i)->content->val, "unset", 6) == 0))
 		unset_builtin(data, i);
 }
--- src/builtins/cd_builtin.c
+++ src/builtins/cd_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:27:23 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/18 13:44:24 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/05/23 13:59:01 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,3 @@ void	cd_builtin(t_data *data, int i)
 	if (chdir(get_n_lst(data->Tokens, i + 1)->content->val) == -1)
 		return ;
 }
-

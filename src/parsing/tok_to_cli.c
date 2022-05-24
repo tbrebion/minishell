dd536b2af20408c47d1f938acc11e9e51f2894dc
Florian CARVALHO <flcarval@student.42.fr>
2022-05-24 17:33:50 +0200

    new tok_to_cli

--- src/parsing/tok_to_cli.c
+++ src/parsing/tok_to_cli.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 15:25:04 by flcarval          #+#    #+#             */
-/*   Updated: 2022/05/24 17:30:44 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/05/24 17:33:38 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ char	**tok_to_cli(t_list **Tokens, int tok_nb)
 			Cli[i] = ft_strjoin(Cli[i], lst->content->val);
 			lst = lst->next;
 		}
+		if (lst)
+			lst = lst->next;
 		i++;
 	}
 	return (Cli);

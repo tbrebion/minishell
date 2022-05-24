87ff6e3a14f76e3b092401e0a72cf5fb1a1a5d11
Titouan BREBION <tbrebion@e1r6p17.42.fr>
2022-05-24 17:19:37 +0200

    redir

--- src/utils/redir.c
+++ src/utils/redir.c
@@ -6,42 +6,41 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:21:49 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/24 17:16:15 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/24 17:19:15 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	redir_in(char *str) //   <
+void	redir_in(char *str)
 {
 	int fd;
 
 	fd = open(str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
-		return (-1); ///return error
+		return ;
 	dup2(fd, 0);
-	return (0);
+	return ;
 }
 
-void	redir_out(char *str) //   >
+void	redir_out(char *str)
 {
 	int	fd;
 
 	fd = open(str,  O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
-		return (-1); ///return error
+		return ;
 	dup2(fd, 1);
-	//exit(0);
-	return (0);
+	return ;
 }
 
-void	redir_out_append(char *str) //   >>
+void	redir_out_append(char *str)
 {
 	int	fd;
 
 	fd = open(str,  O_RDWR | O_CREAT | O_APPEND, 0644);
 	if (fd == -1)
-		return (-1); ///return error
+		return ;
 	dup2(fd, 1);
-	return (0);
+	return ;
 }

b10c494a2d6d5d297075fbf78c162fa96c575960
Titouan BREBION <tbrebion@e2r6p15.clusters.42paris.fr>
2022-05-24 13:27:05 +0200

    wip

--- src/utils/redir.c
+++ src/utils/redir.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:21:49 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/05/24 12:38:35 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/05/24 13:26:38 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	redir_out(char *str) //   >
 	fd = open(str,  O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (-1); ///return error
-	dup2(fd, 0);
+	dup2(fd, 1);
 	return (0);
 }
 

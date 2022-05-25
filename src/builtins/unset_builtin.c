6d810b78599b81084130fc09d572275dfffe6d82
Florian CARVALHO <flcarval@student.42.fr>
2022-05-25 14:29:51 +0200

    unset wip

--- src/builtins/unset_builtin.c
+++ src/builtins/unset_builtin.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 09:33:36 by flcarval          #+#    #+#             */
-/*   Updated: 2022/05/25 12:37:03 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/05/25 14:28:49 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,45 @@
 // 	}
 // }
 
+static void	free_split(char **spl);
+
 void	unset_builtin(t_data *data, int i)
 {
-	int	j;
+	int		j;
+	char	**spl;
+	char	*var;
 
 	j = 0;
-	while (my_env[j])
+	spl = NULL;
+	var = get_n_lst(data->Tokens, i)->content->val;
+	while (data->my_env[j])
 	{
-		
+		spl = ft_split(data->my_env[j], '=');
+		if (!strncmp(var, spl[0], ft_strlen(var) + 1))
+		{
+			while (data->my_env[j])
+			{
+				if (data->my_env[j + 1])
+					data->my_env[j] = data->my_env[j + 1];
+				j++;
+			}
+			data->my_env[j] = NULL;
+			return ;
+		}
+		free_split(spl);
 		j++;
 	}
+}
+
+static void	free_split(char **spl)
+{
+	int	i;
+
+	i = 0;
+	while (spl[i])
+	{
+		free(spl[i]);
+		i++;
+	}
+	free(spl);
 }

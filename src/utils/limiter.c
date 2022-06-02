7b0d47220da3bc51499abe46d310b339f8549908
tbrebion <tbrebion@student.42.fr>
2022-06-02 18:50:07 +0200

    wip

--- src/utils/limiter.c
+++ src/utils/limiter.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 11:02:39 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/06/01 16:47:15 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/06/02 17:39:05 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,19 @@
 /*
 	#####  USE UNLINK  ########
 */
-/*
-void	here_doc(char *limiter)
-{
-	char	*line;
-	char	*ret;
-	int		pid;
-	//int		status;
-	int		i;
 
-	ret = NULL;
-	i = 0;
-	signal(SIGINT, SIG_IGN);
-	pid = fork();
-	if (pid == 0)
-	{
-		signal(SIGINT, interrupt_here_doc);
-		while (1)
-		{
-			ft_putstr("here_doc> ");
-			line = get_next_line(0);
-			//line = readline("here_doc> ");
-			if ((ft_strncmp(line, limiter, ft_max((ft_strlen(line) - 1), ft_strlen(limiter))) == 0))
-			{
-				free(line);
-				break ;
-			}
-			ret = ft_strjoin(ret, line);
-			free(line);
-		}
-		ft_putstr(ret);
-		free(ret);
-		exit(0);
-	}
-	waitpid(-1, NULL, 0);
-}
-*/
 static void	here_doc_supply(char *limiter)
 {
 	char	*line;
 	char	*ret;
 
+	ret = ft_strdup("");
 	signal(SIGINT, interrupt_here_doc);
 	while (1)
 	{
-		/*ft_putstr("here_doc> ");
-		line = get_next_line(0);*/
-		line = readline("here_doc> ");
+		ft_putstr("here_doc> ");
+		line = get_next_line(0);
+		//line = readline("here_doc> ");
 		if ((ft_strncmp(line, limiter, ft_max((ft_strlen(line) - 1), ft_strlen(limiter))) == 0))
 		{
 			free(line);
--- src/utils/signal_manager.c
+++ src/utils/signal_manager.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:14:37 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/06/01 17:43:49 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/06/02 17:38:32 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,5 @@ void	interrupt_here_doc(int signo)
 	data.error_status = 130;
 	set_error_env();
 	//kill(0, SIGINT);
-	exit(130);
+	exit(0);
 }

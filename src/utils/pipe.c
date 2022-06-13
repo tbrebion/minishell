69fc9de23df37d76d3a1a2d2e5cbd20f33d991c3
Titouan BREBION <tbrebion@e1r4p19.42.fr>
2022-06-13 15:32:49 +0200

    wip

--- src/utils/pipe.c
+++ src/utils/pipe.c
@@ -6,7 +6,7 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:30:45 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/06/09 11:38:11 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/06/13 15:31:03 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@
 static void	child_process(int i)
 {
 	int	pid;
+	int status;
 	int	fd[2];
 
 	if (pipe(fd) == -1)
@@ -41,22 +42,26 @@ static void	child_process(int i)
 			execute(i);
 		else
 			builtin_manager(i);
+		exit(0);
 	}
 	else
 	{
 		close(fd[1]);
 		dup2(fd[0], STDIN_FILENO);
 		redir_manager(&data);
-		waitpid(pid, NULL, 0);
+		waitpid(-1, &status, 0);
+		data.error_status = WEXITSTATUS(status);
 	}
 }
 
 void	pipe_cmd(void)
 {
 	int	i;
+	int	status;
 
 	i = -1;
 	while (data.all_cmd[i++])
 		child_process(i);
-	wait(0);
+	waitpid(-1, &status, 0);
+	data.error_status = WEXITSTATUS(status);
 }

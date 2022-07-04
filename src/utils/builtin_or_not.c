3235622f6c0b43fb9898f5610e9fd8bac9b11c37
Florian CARVALHO <flcarval@student.42.fr>
2022-07-04 14:13:29 +0200

    builtin_or_not normed

--- src/utils/builtin_or_not.c
+++ src/utils/builtin_or_not.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_or_not.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 13:19:02 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/07/03 15:46:04 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/07/04 14:12:22 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static void	builtin_or_not_supply(int *status);
 
 void	builtin_or_not(void)
 {
@@ -24,21 +26,24 @@ void	builtin_or_not(void)
 			return ;
 		}
 		else
-		{
-			ignore_sig();
-			data.pid = fork();
-			if (data.pid == 0)
-			{
-				reinit_sig();
-				redir_manager(&data);
-				if (is_builtin(data.lst->content->val) == 0)
-					execute(0);
-				else
-					builtin_manager(0);
-				exit(0);
-			}
-			waitpid(-1, &status, 0);
-			data.error_status = WEXITSTATUS(status);
-		}
+			builtin_or_not_supply(&status);
 	}
+}
+
+static void	builtin_or_not_supply(int *status)
+{
+	ignore_sig();
+	data.pid = fork();
+	if (data.pid == 0)
+	{
+		reinit_sig();
+		redir_manager(&data);
+		if (is_builtin(data.lst->content->val) == 0)
+			execute(0);
+		else
+			builtin_manager(0);
+		exit(0);
+	}
+	waitpid(-1, status, 0);
+	data.error_status = WEXITSTATUS(*status);
 }

40c429ccd08c01c02ff86e3164a5279618871f78
Florian CARVALHO <flcarval@student.42.fr>
2022-07-04 13:57:02 +0200

    echo normed

--- src/builtins/echo_builtin.c
+++ src/builtins/echo_builtin.c
@@ -3,44 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   echo_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 01:26:03 by flcarval          #+#    #+#             */
-/*   Updated: 2022/07/03 15:56:23 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:54:55 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-/*
-echo function takes *str as the string to output
-and n so n = 1 -> print w/ \n (n = 0 -> print w/o \n)
-*/
-
-// ! SEGFAULT  when no args
+static int	echo_empty(void);
+static void	set_n_i(int *n, int *i, int mod);
+static void	echo_supply_loop(int i, int *j);
 
 int	echo_builtin(int i)
 {
 	char	*nl;
-	char	*ret;
 	int		n;
 	int		j;
 
 	if (!(get_n_lst(data.Tokens, i + 1)))
-	{
-		write(1, "\n", 1);
-		return (-1);
-	}
-	if ((ft_strncmp(get_n_lst(data.Tokens, i + 1)->content->val, "-n", 2) == 0))
-	{
-		n = 0;
-		i += 2;
-	}
-	else
-	{
-		n = 1;
-		i += 1;
-	}
+		return (echo_empty());
+	set_n_i(&n, &i, (ft_strncmp(get_n_lst(data.Tokens, i + 1)->content->val, \
+		"-n", 2)));
 	if (n)
 		nl = "\n";
 	else
@@ -50,21 +35,46 @@ int	echo_builtin(int i)
 		(get_n_lst(data.Tokens, i + j)->content->type == I_LITERAL || \
 		get_n_lst(data.Tokens, i + j)->content->type == I_D_QUOTE || \
 		get_n_lst(data.Tokens, i + j)->content->type == I_S_QUOTE))
-	{
-		ret = NULL;
-		if (get_n_lst(data.Tokens, i + j)->content->type != I_S_QUOTE)
-			ret = catch_env_var(get_n_lst(data.Tokens, i + j)->content->val);
-		if (ret != NULL)
-			ft_printf("%s", ret);
-		else
-			ft_printf("%s", get_n_lst(data.Tokens, i + j)->content->val);
-		j++;
-		if (get_n_lst(data.Tokens, i + j) && \
-		(get_n_lst(data.Tokens, i + j)->content->type == I_LITERAL || \
-		get_n_lst(data.Tokens, i + j)->content->type == I_D_QUOTE || \
-		get_n_lst(data.Tokens, i + j)->content->type == I_S_QUOTE))
-			ft_printf(" ");
-	}
+		echo_supply_loop(i, &j);
 	ft_printf("%s", nl);
 	return (0);
+}
+
+static int	echo_empty(void)
+{
+	write(1, "\n", 1);
+	return (-1);
+}
+
+static void	set_n_i(int *n, int *i, int mod)
+{
+	if (!mod)
+	{
+		*n = 0;
+		*i += 2;
+	}
+	else
+	{
+		*n = 1;
+		*i += 1;
+	}
+}
+
+static void	echo_supply_loop(int i, int *j)
+{
+	char	*ret;
+
+	ret = NULL;
+	if (get_n_lst(data.Tokens, i + *j)->content->type != I_S_QUOTE)
+		ret = catch_env_var(get_n_lst(data.Tokens, i + *j)->content->val);
+	if (ret != NULL)
+		ft_printf("%s", ret);
+	else
+		ft_printf("%s", get_n_lst(data.Tokens, i + *j)->content->val);
+	(*j)++;
+	if (get_n_lst(data.Tokens, i + *j) && \
+	(get_n_lst(data.Tokens, i + *j)->content->type == I_LITERAL || \
+	get_n_lst(data.Tokens, i + *j)->content->type == I_D_QUOTE || \
+	get_n_lst(data.Tokens, i + *j)->content->type == I_S_QUOTE))
+		ft_printf(" ");
 }

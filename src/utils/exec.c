5554d2cb84f1a079511890823f08ad2190c5d68d
Florian CARVALHO <flcarval@student.42.fr>
2022-07-28 15:23:37 +0200

    need norm

--- src/utils/exec.c
+++ src/utils/exec.c
@@ -6,14 +6,13 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 09:45:14 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/07/28 15:12:00 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/07/28 15:23:29 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 static void	execute_supply(char *path, char **cmd);
-// static void
 
 char	*find_path(char *cmd, char **my_paths)
 {
